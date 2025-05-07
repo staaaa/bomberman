078d8bfb21b6c485fba14ba3027db1c8786a9064
kbkl <j.kalinowskyy@gmail.com>
2025-05-07 16:16:12 +0200

    Small map adjustment

--- common/map.c
+++ common/map.c
@@ -13,21 +13,23 @@ Map initialize_map_from_file(const char* filename, int grid_size) {
 
     int width = 0, height = 0;
     char buffer[1024];
-    
-    if (fgets(buffer, sizeof(buffer), file)) {
-        char* token = strtok(buffer, " \n");
-        while (token) {
-            width++;
-            token = strtok(NULL, " \n");
-        }
-    }
-    
+    char* lines[512];
+    int line_count = 0;
+
     while (fgets(buffer, sizeof(buffer), file)) {
-        height++;
+        lines[line_count] = strdup(buffer);
+        if (line_count == 0) {
+            char* token = strtok(buffer, " \n");
+            while (token) {
+                width++;
+                token = strtok(NULL, " \n");
+            }
+        }
+        line_count++;
     }
-    
-    rewind(file);
-    
+
+    height = line_count;
+
     map.width = width;
     map.height = height;
     map.tile_arr = malloc(height * sizeof(Tile*));
@@ -36,17 +38,14 @@ Map initialize_map_from_file(const char* filename, int grid_size) {
     }
 
     for (int y = 0; y < height; y++) {
-        if (!fgets(buffer, sizeof(buffer), file)) {
-            break;
-        }
-        
-        char* token = strtok(buffer, " \n");
+        char* token = strtok(lines[y], " \n");
         for (int x = 0; x < width && token; x++) {
             map.tile_arr[y][x].x = x;
             map.tile_arr[y][x].y = y;
             map.tile_arr[y][x].type = atoi(token);
             token = strtok(NULL, " \n");
         }
+        free(lines[y]);
     }
 
     fclose(file);
